33b68990a3d76471d6da6abb789d5c9ddbe063ce
teukusulthan <teukusultanul@gmail.com>
2025-08-20 14:44:30 +0700

    feat(products): add POST /products endpoint

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -1,6 +1,21 @@
 import { Prisma } from "@prisma/client";
 import { Request, Response } from "express";
 import { prisma } from "../connection/client";
+import { error } from "console";
+
+export const getProduct = async (req: Request, res: Response) => {
+  try {
+    const id = parseInt(req.params.id);
+    const product = await prisma.product.findUnique({ where: { id } });
+
+    if (!product) {
+      res.status(404).json({ error: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch datas" });
+  }
+};
 
 export const getProducts = async (req: Request, res: Response) => {
   try {
@@ -8,5 +23,17 @@ export const getProducts = async (req: Request, res: Response) => {
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch datas" });
+  }
+};
+
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const { name, price } = req.body;
+    const product = await prisma.product.create({
+      data: { name, price: parseFloat(price) },
+    });
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create product" });
   }
 };
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,8 +1,14 @@
 import express from "express";
-import { getProducts } from "../controllers/product-controller";
+import {
+  createProduct,
+  getProducts,
+  getProduct,
+} from "../controllers/product-controller";
 
 const router = express.Router();
 
 router.get("/products", getProducts);
+router.get("/product/:id", getProduct);
+router.post("/product", createProduct);
 
 export default router;
